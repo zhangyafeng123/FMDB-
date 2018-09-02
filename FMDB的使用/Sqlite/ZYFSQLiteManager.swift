5b39d9302cdf3303522b8a6d161c31c6ff5655ba
张亚峰 <1240334911@qq.com>
2018-09-02 18:24:21 +0800

    添加 fmdb框架，建立数据库管理单例

---
+++ FMDB的使用/Sqlite/ZYFSQLiteManager.swift
@@ -0,0 +1,22 @@
+//
+//  ZYFSQLiteManager.swift
+//  FMDB的使用
+//
+//  Created by 张亚峰 on 2018/9/2.
+//  Copyright © 2018年 zhangyafeng. All rights reserved.
+//
+
+import UIKit
+
+/// sqlite单例
+class ZYFSQLiteManager {
+    
+    /// 单例，全局数据库工具访问点
+    static let shared = ZYFSQLiteManager()
+    
+    /// 构造函数
+    private init() {
+        
+    }
+    
+}
